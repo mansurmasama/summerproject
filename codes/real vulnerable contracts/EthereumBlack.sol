2a0fdff5f1b4c8a634fa523c1c4fc950203f8366
mansurmasama <mansurmasama@gmail.com>
2019-08-05 16:57:02 +0100

    Update EthereumBlack.sol

--- codes/real vulnerable contracts/EthereumBlack.sol
+++ codes/real vulnerable contracts/EthereumBlack.sol
@@ -5,275 +5,8 @@
 //ERC20 Token
 pragma solidity ^0.4.2;
 //import "browser/SafeMath8.sol";
-library SafeMath {
 
-    /**
 
-     * @dev Returns the addition of two unsigned integers, reverting on
-
-     * overflow.
-
-     *
-
-     * Counterpart to Solidity's `+` operator.
-
-     *
-
-     * Requirements:
-
-     * - Addition cannot overflow.
-
-     */
-
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        uint256 c = a + b;
-
-        require(c >= a);
-
-
-
-        return c;
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the subtraction of two unsigned integers, reverting on
-
-     * overflow (when the result is negative).
-
-     *
-
-     * Counterpart to Solidity's `-` operator.
-
-     *
-
-     * Requirements:
-
-     * - Subtraction cannot overflow.
-
-     */
-
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        return sub(a, b);
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
-
-     * overflow (when the result is negative).
-
-     *
-
-     * Counterpart to Solidity's `-` operator.
-
-     *
-
-     * Requirements:
-
-     * - Subtraction cannot overflow.
-
-     */
-
-    function sub2(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        require(b <= a);
-
-        uint256 c = a - b;
-
-
-
-        return c;
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the multiplication of two unsigned integers, reverting on
-
-     * overflow.
-
-     *
-
-     * Counterpart to Solidity's `*` operator.
-
-     *
-
-     * Requirements:
-
-     * - Multiplication cannot overflow.
-
-     */
-
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
-
-        // benefit is lost if 'b' is also tested.
-
-        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
-
-        if (a == 0) {
-
-            return 0;
-
-        }
-
-
-
-        uint256 c = a * b;
-
-        require(c / a == b);
-
-
-
-        return c;
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the integer division of two unsigned integers. Reverts on
-
-     * division by zero. The result is rounded towards zero.
-
-     *
-
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-
-     * uses an invalid opcode to revert (consuming all remaining gas).
-
-     *
-
-     * Requirements:
-
-     * - The divisor cannot be zero.
-
-     */
-
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        return div(a, b);
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
-
-     * division by zero. The result is rounded towards zero.
-
-     *
-
-     * Counterpart to Solidity's `/` operator. Note: this function uses a
-
-     * `revert` opcode (which leaves remaining gas untouched) while Solidity
-
-     * uses an invalid opcode to revert (consuming all remaining gas).
-
-     *
-
-     * Requirements:
-
-     * - The divisor cannot be zero.
-
-     */
-
-    function div2(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        // Solidity only automatically asserts when dividing by 0
-
-        require(b > 0);
-
-        uint256 c = a / b;
-
-        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-
-
-
-        return c;
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-
-     * Reverts when dividing by zero.
-
-     *
-
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-
-     * invalid opcode to revert (consuming all remaining gas).
-
-     *
-
-     * Requirements:
-
-     * - The divisor cannot be zero.
-
-     */
-
-    function mod(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        return mod(a, b);
-
-    }
-
-
-
-    /**
-
-     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
-
-     * Reverts with custom message when dividing by zero.
-
-     *
-
-     * Counterpart to Solidity's `%` operator. This function uses a `revert`
-
-     * opcode (which leaves remaining gas untouched) while Solidity uses an
-
-     * invalid opcode to revert (consuming all remaining gas).
-
-     *
-
-     * Requirements:
-
-     * - The divisor cannot be zero.
-
-     */
-
-    function mod2(uint256 a, uint256 b) internal constant returns (uint256) {
-
-        require(b != 0);
-
-        return a % b;
-
-    }
-
-}
 contract owned {
     address public owner;
 
@@ -301,7 +34,7 @@ contract token {
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
-    using SafeMath for uint256;
+    //using SafeMath for uint256;
     /* This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
@@ -422,17 +155,17 @@ contract ETHEREUMBLACK is owned, token {
     }
 
     function mintToken(address target, uint256 mintedAmount) onlyOwner {
-        //balanceOf[target] += mintedAmount;
+        balanceOf[target] += mintedAmount;
         
         //uint256 temp=balanceOf[target]; // temp varible for SafeMath8 expression
         //balanceOf[target] =balanceOf[target]+mintedAmount; //SafeMath8 expression
         //balanceOf[target].addmult(balanceOf[target],mintedAmount); //SafeMath8 function call
         
-        balanceOf[target] = balanceOf[target].add(mintedAmount); //SafeMath expression and function call
+        //balanceOf[target] = balanceOf[target].add(mintedAmount); //SafeMath expression and function call
 
         
-        //totalSupply += mintedAmount;
-         totalSupply=totalSupply.add(mintedAmount);
+        totalSupply += mintedAmount;
+        // totalSupply=totalSupply.add(mintedAmount);
         //Transfer(0, this, mintedAmount);
         //Transfer(this, target, mintedAmount);
         Transfer(0, this, mintedAmount);
