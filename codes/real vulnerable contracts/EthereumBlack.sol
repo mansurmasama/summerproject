52f274d76b06c5778d8922e5201744e52381c3e0
mansurmasama <mansurmasama@gmail.com>
2019-08-05 09:22:14 +0100

    Update EthereumBlack.sol

--- codes/real vulnerable contracts/EthereumBlack.sol
+++ codes/real vulnerable contracts/EthereumBlack.sol
@@ -4,6 +4,277 @@
 
 //ERC20 Token
 pragma solidity ^0.4.2;
+
+library SafeMath {
+
+    /**
+
+     * @dev Returns the addition of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `+` operator.
+
+     *
+
+     * Requirements:
+
+     * - Addition cannot overflow.
+
+     */
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        uint256 c = a + b;
+
+        require(c >= a);
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+
+     * overflow (when the result is negative).
+
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return sub(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+
+     * overflow (when the result is negative).
+
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b <= a);
+
+        uint256 c = a - b;
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `*` operator.
+
+     *
+
+     * Requirements:
+
+     * - Multiplication cannot overflow.
+
+     */
+
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+
+        // benefit is lost if 'b' is also tested.
+
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+
+        if (a == 0) {
+
+            return 0;
+
+        }
+
+
+
+        uint256 c = a * b;
+
+        require(c / a == b);
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return div(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Solidity only automatically asserts when dividing by 0
+
+        require(b > 0);
+
+        uint256 c = a / b;
+
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return mod(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts with custom message when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b != 0);
+
+        return a % b;
+
+    }
+
+}
+
 contract owned {
     address public owner;
 
@@ -57,10 +328,10 @@ contract token {
     function transfer(address _to, uint256 _value) {
         //if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
         require(balanceOf[msg.sender] < _value);
-        //if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        require (balanceOf[_to] + _value <balanceOf[_to]);
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
+        //if (balanceOf[_to].add(_value) < balanceOf[_to]) throw; // Check for overflows
+        require (balanceOf[_to].add(_value) <balanceOf[_to]);
+        balanceOf[msg.sender] =balanceOf[msg.sender].sub(_value);                     // Subtract from the sender
+        balanceOf[_to] = balanceOf[to].sub(_value);                            // Add the same to the recipient
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
 
@@ -85,12 +356,12 @@ contract token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         //if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
         require(balanceOf[_from] < _value);
-        //if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        require(balanceOf[_to] + _value < balanceOf[_to]);
+        //if (balanceOf[_to].add(_value) < balanceOf[_to]) throw;  // Check for overflows
+        require(balanceOf[_to].add(_value)balanceOf[_to]);
         //if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
         require(_value > allowance[_from][msg.sender]);
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
+        balanceOf[_from] = balanceOf[_from].sub(_value);                          // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].sub(_value);                            // Add the same to the recipient
         allowance[_from][msg.sender] -= _value;
         Transfer(_from, _to, _value);
         return true;
