d364a08169edcbc683e022f5664d1394aff94bca
mansurmasama <mansurmasama@gmail.com>
2019-08-10 18:22:02 +0100

    Update PolyAi2.sol

--- codes/real vulnerable contracts/PolyAi2.sol
+++ codes/real vulnerable contracts/PolyAi2.sol
@@ -1,37 +1,273 @@
 //Compiler Version: v0.4.14+commit.c2215d46 Vulnerable function: 136-140
 pragma solidity ^0.4.2;
-//pragma solidity ^0.5.0;
+
 library SafeMath {
 
+    /**
+
+     * @dev Returns the addition of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `+` operator.
+
+     *
+
+     * Requirements:
+
+     * - Addition cannot overflow.
+
+     */
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        uint256 c = a + b;
+
+        require(c >= a);
 
 
-     //merging add and mul functions 
 
-    function addmult(uint256 c,uint256 a, uint256 b) internal constant {
+        return c;
 
-
-
-    
-
-     require(c >= a && c >= b);
-
-}
-
-   
-
-     // merging mod and div and sub functions
-
-    function moddivsub(uint256 c, uint256 a, uint256 b  ) internal constant  {
+    }
 
 
 
-        require(c<=a && b > 0);
+    /**
 
-        
+     * @dev Returns the subtraction of two unsigned integers, reverting on
 
-           }
+     * overflow (when the result is negative).
 
-    
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return sub(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+
+     * overflow (when the result is negative).
+
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b <= a);
+
+        uint256 c = a - b;
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `*` operator.
+
+     *
+
+     * Requirements:
+
+     * - Multiplication cannot overflow.
+
+     */
+
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+
+        // benefit is lost if 'b' is also tested.
+
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+
+        if (a == 0) {
+
+            return 0;
+
+        }
+
+
+
+        uint256 c = a * b;
+
+        require(c / a == b);
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return div(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Solidity only automatically asserts when dividing by 0
+
+        require(b > 0);
+
+        uint256 c = a / b;
+
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return mod(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts with custom message when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b != 0);
+
+        return a % b;
+
+    }
 
 }
 
@@ -85,9 +321,9 @@ contract token {
     /* Send coins */
     function transfer(address _to, uint256 _value) {
         if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
+        if (balanceOf[_to].add(_value) < balanceOf[_to]) revert(); // Check for overflows
+        balanceOf[msg.sender] =balanceOf[msg.sender].sub(_value);                     // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].add(_value);                            // Add the same to the recipient
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
 
@@ -111,11 +347,11 @@ contract token {
     /* A contract attempts to get the coins */
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
+        if (balanceOf[_to].add(_value) < balanceOf[_to]) revert();  // Check for overflows
         if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
+        balanceOf[_from] =balanceOf[_from].sub(_value);                          // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].add(_value);                            // Add the same to the recipient
+        allowance[_from][msg.sender] =   allowance[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
@@ -144,10 +380,10 @@ contract PolyAi is owned, token {
     /* Send coins */
     function transfer(address _to, uint256 _value) {
         if (balanceOf[msg.sender] < _value) revert();           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert(); // Check for overflows
+        if (balanceOf[_to] .add(_value) < balanceOf[_to]) revert(); // Check for overflows
         if (frozenAccount[msg.sender]) revert();                // Check if frozen
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
+        balanceOf[msg.sender] =balanceOf[msg.sender].sub(_value);                     // Subtract from the sender
+        balanceOf[_to] = balanceOf[_to].add(_value);                            // Add the same to the recipient
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
 
@@ -155,25 +391,20 @@ contract PolyAi is owned, token {
     function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
         if (frozenAccount[_from]) revert();                        // Check if frozen            
         if (balanceOf[_from] < _value) revert();                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) revert();  // Check for overflows
+        if (balanceOf[_to] .add(_value) < balanceOf[_to]) revert();  // Check for overflows
         if (_value > allowance[_from][msg.sender]) revert();   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
+        balanceOf[_from] = balanceOf[_from].sub(_value);                          // Subtract from the sender
+        balanceOf[_to] =  balanceOf[_to].add(_value);                            // Add the same to the recipient
+        allowance[_from][msg.sender] =allowance[_from][msg.sender].sub(_value);
         Transfer(_from, _to, _value);
         return true;
     }
 
     function mintToken(address target, uint256 mintedAmount) onlyOwner {
-        //balanceOf[target] += mintedAmount ; // initial contract expression
-        
-        uint256 temp=balanceOf[target]; // temp varible for SafeMath8 expression
-        balanceOf[target] =temp+mintedAmount; //SafeMath8 expression
-        balanceOf[target].addmult(temp,mintedAmount); //SafeMath8 function call
-        //the above three expressions severely affects the gas cost to rise over SafeMath
-        
-        //balanceOf[target] = balanceOf[target].add(mintedAmount); //SafeMath expression and function call
-        
+     
+
+        balanceOf[target] = balanceOf[target].add(mintedAmount); //SafeMath expression and function call
+   
         Transfer(0, owner, mintedAmount);
         Transfer(owner, target, mintedAmount);
     }
