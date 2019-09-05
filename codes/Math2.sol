cb9fd166f32e6183476234998d267c8936172e54
mansurmasama <mansurmasama@gmail.com>
2019-09-05 13:25:00 +0100

    Update Math2.sol

--- codes/Math2.sol
+++ codes/Math2.sol
@@ -1,48 +1,317 @@
-//testing the execution cost and transaction cost without SafeMath library
-pragma solidity ^0.5.0;
-//import "browser/SafeMath.sol";
+//with original SafeMath library 
+
+pragma solidity ^0.4.2;
+library SafeMath {
+
+    /**
+
+     * @dev Returns the addition of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `+` operator.
+
+     *
+
+     * Requirements:
+
+     * - Addition cannot overflow.
+
+     */
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        uint256 c = a + b;
+
+        require(c >= a);
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+
+     * overflow (when the result is negative).
+
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return sub2(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+
+     * overflow (when the result is negative).
+
+     *
+
+     * Counterpart to Solidity's `-` operator.
+
+     *
+
+     * Requirements:
+
+     * - Subtraction cannot overflow.
+
+     */
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b <= a);
+
+        uint256 c = a - b;
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+
+     * overflow.
+
+     *
+
+     * Counterpart to Solidity's `*` operator.
+
+     *
+
+     * Requirements:
+
+     * - Multiplication cannot overflow.
+
+     */
+
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+
+        // benefit is lost if 'b' is also tested.
+
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+
+        if (a == 0) {
+
+            return 0;
+
+        }
+
+
+
+        uint256 c = a * b;
+
+        require(c / a == b);
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return div2(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+
+     * division by zero. The result is rounded towards zero.
+
+     *
+
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+
+     * uses an invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        // Solidity only automatically asserts when dividing by 0
+
+        require(b > 0);
+
+        uint256 c = a / b;
+
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+
+
+        return c;
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod2(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        return mod2(a, b);
+
+    }
+
+
+
+    /**
+
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+
+     * Reverts with custom message when dividing by zero.
+
+     *
+
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+
+     * invalid opcode to revert (consuming all remaining gas).
+
+     *
+
+     * Requirements:
+
+     * - The divisor cannot be zero.
+
+     */
+
+    function mod(uint256 a, uint256 b) internal constant returns (uint256) {
+
+        require(b != 0);
+
+        return a % b;
+
+    }
+
+}
 
 
 contract Maths {
-   // using SafeMath for uint256;
+    using SafeMath for uint256;
     uint256 public total;
     
-    function Math() public pure {
+    function Math() public constant {
         //constructor
     }
     
     //multiplication operation
-    function pewPew(uint256 x, uint y) public  {
+    function multiplication(uint256 x, uint256 y) public  {
 
-    total =x*(y);
+    total =x.mul(y);
 
 }
 
     // addition operations
-    function additions(uint256 x, uint y) public  {
+    function additions(uint256 x, uint256 y) public  {
 
-    total =x+(y);
+    total =x.add(y);
 
 }
 
     //substraction operation
-    function substraction(uint256 x, uint y) public  {
+    function substraction(uint256 x, uint256 y) public  {
 
-    total =x-(y);
+    total =x.sub(y);
 
 }
 
     //modulus operation
-    function modulus(uint256 x, uint y) public  {
+    function modulus(uint256 x, uint256 y) public  {
 
-    total =x**(y);
+    total =x.mod(y);
 
 }
 
    //division operation
-    function division(uint256 x, uint y) public  {
+    function division(uint256 x, uint256 y) public  {
 
-    total =x/(y);
+    total =x.div(y);
 
 }
     
